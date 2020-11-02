6066cdc6a967e247a1512c09c4a8110fd112f45b
sterlingku <sterling.ku@gmail.com>
2020-11-01 16:46:37 -0800

    Update server.py

--- server.py
+++ server.py
@@ -1,11 +1,6 @@
 from os import environ
 from flask import Flask
-import os
+
 
 app = Flask(__name__)
-app.run(environ.get('PORT'))
-
-if __name__ == '__main__':
-    # Bind to PORT if defined, otherwise default to 5000.
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+app.run(host='0.0.0.0', port=environ.get('PORT'))
