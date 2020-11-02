834f0c3bb567df40e1d4144d2742620310fc9f9a
sterlingku <sterling.ku@gmail.com>
2020-11-02 15:03:33 -0800

    c

--- Respond-Bot/respond-bot.py
+++ Respond-Bot/respond-bot.py
@@ -2,19 +2,19 @@
 
 import time
 import tweepy
-# import os
-# from dotenv import load_dotenv
-from os import environ
 
 
 # testing locally via .env file
+# import os
+# from dotenv import load_dotenv
 # load_dotenv()
 # API_CONSUMER_KEY = os.getenv('API_CONSUMER_KEY')
 # API_CONSUMER_SECRET = os.getenv('API_CONSUMER_SECRET')
 # ACCESS_TOKEN_KEY = os.getenv('ACCESS_TOKEN_KEY')
 # ACCESS_TOKEN_SECRET = os.getenv('ACCESS_TOKEN_SECRET')
 
-# stores the API keys somewhere else, so people can't find it in your code.  It is in Heroku Config Vars
+# stores the API keys in Heroku Config Vars
+from os import environ
 API_CONSUMER_KEY = environ['API_CONSUMER_KEY']
 API_CONSUMER_SECRET = environ['API_CONSUMER_SECRET']
 ACCESS_TOKEN_KEY = environ['ACCESS_TOKEN_KEY']
@@ -29,23 +29,29 @@ file_name = 'last_seen.txt'
 class Tweet:
 
     def reply_to_tweets(self):
-        print('Retrieving and replying to tweets...')
-        last_id = self.retrieve_seen_id()
-        # all mentions since the latest last_id
-        mentions = api.mentions_timeline(last_id, tweet_mode='extended', count=5000)
-        # iterate through each mention in reverse to reply to older tweets first
-        for mention in reversed(mentions):
-            print(mention)
-            # reply to tweets that meet criteria below
-            if '#helloworld!' in mention.full_text.lower():
-                self.store_seen_id(mention.id)
-                print(str(mention.id) + ' - ' + mention.user.screen_name + ' - ' + mention.full_text)
-                print('Found #helloworld! Responding back...')
-                # prints an error, if any
-                print(tweepy.error.TweepError)
-                api.update_status('@' + mention.user.screen_name + ' HelloWorld back to you!', mention.id)
-                # sleep for every 5 seconds
-                time.sleep(10)
+        # while loop required to constantly run
+        while True:
+            print('Retrieving and replying to tweets...')
+            last_id = self.retrieve_seen_id()
+            # all mentions since the latest last_id
+            mentions = api.mentions_timeline(last_id, tweet_mode='extended', count=5000)
+            # iterate through each mention in reverse to reply to older tweets first
+            for mention in reversed(mentions):
+                print(mention)
+                # reply to tweets that meet criteria below
+                if '#helloworld!' in mention.full_text.lower():
+                    self.store_seen_id(mention.id)
+                    print(str(mention.id) + ' - ' + mention.user.screen_name + ' - ' + mention.full_text)
+                    print('Found #helloworld! Responding back...')
+                    # prints an error, if any
+                    print(tweepy.error.TweepError)
+                    api.update_status('@' + mention.user.screen_name + ' HelloWorld back to you!', mention.id)
+                    # sleep for 5 seconds before replying
+                    time.sleep(5)
+                else:
+                    print('No new tweets.  Running again soon...')
+            # sleep for 10 seconds before re-running
+            time.sleep(10)
 
     # store the seen_id, so bot knows where to continue from next time
     def store_seen_id(self, seen_id):
@@ -65,8 +71,5 @@ class Tweet:
 
 
 # driver code needed for this to work
-# while loop needed to run script constantly
-while True:
-    obj1 = Tweet()
-    obj1.reply_to_tweets()
-    time.sleep(10)
+obj1 = Tweet()
+obj1.reply_to_tweets()
